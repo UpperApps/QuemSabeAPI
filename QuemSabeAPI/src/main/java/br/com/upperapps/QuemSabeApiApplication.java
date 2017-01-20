68b58a434bf705ea587faaabea4a9b8351231839
rodrigomelo <rodrigomelo@192.168.0.114>
2017-01-20 17:46:30 -0200

    Commit inicial do projeto.

---
+++ QuemSabeAPI/src/main/java/br/com/upperapps/QuemSabeApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.upperapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuemSabeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuemSabeApiApplication.class, args);
+	}
+}
